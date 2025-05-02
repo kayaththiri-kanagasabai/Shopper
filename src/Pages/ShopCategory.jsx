5a7a892f17d7a3855210c6d378f08f6a5e1ba49d
kayaththiri-kanagasabai <kaya282002@gmail.com>
2025-05-02 22:09:29 +0530

    commit remaining components foleders

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import './CSS/ShopCategory.css'
+import { ShopContext } from '../Context/ShopContext'
+import dropdown_icon from '../Components/Assets/dropdown_icon.png'
+import Item from '../Components/Item/Item'
 
-const ShopCategory = () => {
+const ShopCategory = (props) => {
+  const { all_product } = useContext(ShopContext)
+  console.log(all_product)
   return (
-    <div>ShopCategory</div>
+    <div className='shop-category'>
+      <img className='shopcategory-banner' src={props.banner} alt="" />
+
+      <div className="shopcategory-indexSort">
+        <p>
+          <span>Showing 1-12</span> out of 36 products
+        </p>
+
+
+        <div className="shopcategory-sort">
+          Sort by <img src={dropdown_icon} alt="" />
+        </div>
+      </div>
+
+      <div className='shopcategory-loadmore'>
+        Explore More
+      </div>
+    </div>
   )
 }
 
